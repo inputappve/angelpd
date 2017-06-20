3c21cb47e0b9eb8737820ac3b2a0bed9c3bcbded
Mattia Fusaro <mattiafusaro8@gmail.com>
2017-06-20 15:15:04 +0200

    database

--- app-base/app-base/www/app/controller_app_map.js
+++ app-base/app-base/www/app/controller_app_map.js
@@ -1,4 +1,24 @@
 app.mapSrv = function(ctl,NgMap, NavigatorGeolocation, fine,tipo){
+	ctl.mbu = function(userId,marker) {
+  firebase.database().ref('Venezia/' + userId).set({
+     lat:marker.lat,
+     lng:marker.lng,
+     icon: marker.icona,
+     dateStart:marker.inizio,
+     dateFinish:marker.fine
+  });
+}
+	ctl.loadMap = function(){
+		return firebase.database().ref('/Venezia/').once('value').then(function(snapshot) {
+  		var username = snapshot.val().username;
+  		var lat = snapshot.val().lat,
+    	var lng = snapshot.val().lng,
+   		var icona  = snapshot.val().icon,
+    	var inizio = snapshot.val().dateStart,
+  		var fine =snapshot.val().dateFinish
+		});
+	}
+
   ctl.mapSrv = {};
 	ctl.mapSrv = NgMap.getMap();
   console.log("ho recuperato la mappa -> ", ctl.mapSrv);
@@ -18,6 +38,7 @@ app.mapSrv = function(ctl,NgMap, NavigatorGeolocation, fine,tipo){
       fine: new Date(fine)
   	};
   	ctl.mapSrv.markers.push(x);
+  	ctl.mbu(firebase.auth().currentUser.uid,x);
     console.log("Ho aggiunto marker "+tipo);
     console.log(ctl.mapSrv.markers[ctl.mapSrv.markers.length-1]);
   });
