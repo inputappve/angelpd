2503c12bbcdf2a93b8915d123d2ea752bf21454a
Teo <earlscourts99@gmail.com>
2017-06-23 14:29:06 +0200

    alert popup su icone pericolo

--- app-base/app-base/www/app/controller_app.js
+++ app-base/app-base/www/app/controller_app.js
@@ -36,7 +36,7 @@ angular.module('app')
 
   app.app_helper(ctl);
   app.app_login(ctl,passwordcurrent);
-  app.mapSrv(ctl, NgMap,NavigatorGeolocation, Date.now(), new Date(Date.now+100000));
+  app.mapSrv(ctl, NgMap,NavigatorGeolocation, Date.now(), new Date(Date.now+100000),$scope);
 
   ctl.showDialog = function(id){
       $(id).show();
@@ -66,79 +66,145 @@ angular.module('app')
   ctl.msg=[];
   ctl.msg.push({
           titolo: "Pericolo generico",
-          msg: "Vuoi segnalare un pericolo generico?"
+          msg: "Vuoi segnalare un pericolo generico?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Pericolo Incendio",
-          msg: "Vuoi segnalare un pericolo di incendio?"
+          msg: "Vuoi segnalare un pericolo di incendio?",
+          icona: "Signal/burn.png"
            });
   ctl.msg.push({
           titolo: "Presenza Spazzatura",
-          msg: "Vuoi segnalare la presenza di spazzatura fuori dal cestino?"
+          msg: "Vuoi segnalare la presenza di spazzatura fuori dal cestino?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Presenza Rapinatori",
-          msg: "Vuoi segnalare la presenza di rapinatori nell'area?"
+          msg: "Vuoi segnalare la presenza di rapinatori nell'area?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
          titolo: "Presenza Spacciatori",
-         msg: "Vuoi segnalare la presenza di spacciatori nell'area?"
+         msg: "Vuoi segnalare la presenza di spacciatori nell'area?",
+         icona: "Signal/garbage.png"
           });
   ctl.msg.push({
           titolo: "Blackout",
-          msg: "Vuoi segnalare la presenza di un blackout nell'area circostante?"
+          msg: "Vuoi segnalare la presenza di un blackout nell'area circostante?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Vortice",
-          msg: "Vuoi segnalare la presenza di trombe d'aria?"
+          msg: "Vuoi segnalare la presenza di trombe d'aria?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Incidente",
-          msg: "Vuoi segnalare la presenza di incidenti stradali?"
+          msg: "Vuoi segnalare la presenza di incidenti stradali?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Allagamento",
-          msg: "Vuoi segnalare la presenza di zone allagate?"
+          msg: "Vuoi segnalare la presenza di zone allagate?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Folla",
-          msg: "Vuoi segnalare un affollamento di persone?"
+          msg: "Vuoi segnalare un affollamento di persone?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Rissa",
-          msg: "Vuoi segnalare una rissa?"
+          msg: "Vuoi segnalare una rissa?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Lavori in corso",
-          msg: "Vuoi segnalare la presenza di lavori in corso?"
+          msg: "Vuoi segnalare la presenza di lavori in corso?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Esalazioni Tossiche",
-          msg: "Vuoi segnalare la presenza di sostanze tossiche/nocive?"
+          msg: "Vuoi segnalare la presenza di sostanze tossiche/nocive?",
+          icona: "Signal/garbage.png"
            });
   ctl.msg.push({
           titolo: "Terremoto",
-          msg: "Vuoi segnalare la presenza di un terremoto nell'area circostante?"
+          msg: "Vuoi segnalare la presenza di un terremoto nell'area circostante?",
+          icona: "Signal/garbage.png"
            });
- 
+   ctl.icons = {
+    garbage : "Signal/garbage.png",
+    incendio : "Signal/burn.png",
+    pericolo: "Signal/exclamation.png",
+    rapina: "Signal/gun.png",
+    droga:"Signal/droga.png",
+    blackout: "Signal/bolt.png",
+    tornado: "Signal/tornado.png",
+    Incidente: "Signal/incidente.png",
+    Acqua: "Signal/drop.png",
+    gente: "Signal/team.png",
+    botte:"Signal/boxing.png",
+    lavori: "Signal/badile.png",
+    tossico: "Signal/poison.png",
+    terremoto:"Signal/earthquake.png"
+  }; 
 
 
-ctl.icons = {
-  garbage : "Signal/garbage.png",
-  incendio : "Signal/burn.png",
-  pericolo: "Signal/exclamation.png",
-  rapina: "Signal/gun.png",
-  droga:"Signal/droga.png",
-  blackout: "Signal/bolt.png",
-  tornado: "Signal/tornado.png",
-  Incidente: "Signal/incidente.png",
-  Acqua: "Signal/drop.png",
-  gente: "Signal/team.png",
-  botte:"Signal/boxing.png",
-  lavori: "Signal/badile.png",
-  tossico: "Signal/poison.png",
-  terremoto:"Signal/earthquake.png"
-}; 
+  ctl.type_from_icons = {
+    "Signal/garbage.png"     : "Spazzatura", 
+    "Signal/burn.png"        : "Incendio", 
+    "Signal/exclamation.png" : "Pericolo Generico",
+    "Signal/gun.png"         : "Rapina",
+    "Signal/droga.png"       : "Spaccio di droga",
+    "Signal/bolt.png"        : "Blackout",
+    "Signal/tornado.png"     : "Tornado",
+    "Signal/incidente.png"   : "Incidente",
+    "Signal/drop.png"        : "Acqua",
+    "Signal/team.png"        : "Affollamento",
+    "Signal/boxing.png"      : "Rissa",
+    "Signal/badile.png"      : "Lavori in corso",
+    "Signal/poison.png"      : "Esalazioni tossiche",
+    "Signal/earthquake.png"  : "Terremoto" 
+  };
+
+  ctl.messaggi_pericolo = {
+    "Signal/garbage.png"     : "Presenza di spazzatura nella strada", 
+    "Signal/burn.png"        : "Presenza di un incendio nelle vicinanze", 
+    "Signal/exclamation.png" : "C'è la presenza di un pericolo generico, prestare attenzione",
+    "Signal/gun.png"         : "é in corso una rapina!",
+    "Signal/droga.png"       : "Degli spacciatori stanno spacciando droga",
+    "Signal/bolt.png"        : "C'è un Blackout",
+    "Signal/tornado.png"     : "C'è un tornado/tromba d'aria nelle vicinanze",
+    "Signal/incidente.png"   : "C'è stato un icidente lungo la strada",
+    "Signal/drop.png"        : "C'è stato un allagamento",
+    "Signal/team.png"        : "In questo luogo c'è un affolamento di persone",
+    "Signal/boxing.png"      : "é in corso una rissa",
+    "Signal/badile.png"      : "Ci sono dei lavori in corso",
+    "Signal/poison.png"      : "Esalazioni tossiche, restare distanti",
+    "Signal/earthquake.png"  : "C'è stato un terremoto, prestare soccorso" 
+  };
+
+
+  ctl.display_alert  = function(ev,notworking,ok,titolo) {
+      console.log("questo parametro non va ",notworking);
+      console.log("ok-> ",ok);
+      console.log("obj-> ",ctl.type_from_icons[titolo]);
+      console.log("obj-> ",ctl.messaggi_pericolo[titolo]);
+      $mdDialog.show(
+          $mdDialog.alert()
+             .clickOutsideToClose(true)
+             .title(ctl.type_from_icons[titolo])
+             .ariaLabel("Info pericolo")
+             .textContent(ctl.messaggi_pericolo[titolo])
+             .ok(ok)
+             .targetEvent(ev)
+      );
+  }; 
+
+
+
 
   $scope.showConfirm = function(ev, titolo,tipo) {
     var messaggio=null;
@@ -160,27 +226,14 @@ ctl.icons = {
 
     $mdDialog.show(confirm).then(function() {
     //  $scope.status="";
-      ctl.mapSrv.addMarker(Date.now()+60000*30,tipo,app.ctl.icons);
+      ctl.mapSrv.addMarker(Date.now()+60000*30,tipo,app.ctl.icons,firebase.auth().currentUser);
       $scope.toggleRight();
     }, function() {
     //  $scope.status = "";
     });
-  };
+  }//showconfirm
+  
   //
-  ctl.loadMap = function(){
-  var markers = firebase.database().ref("Venezia/");
-  markers.on("child_added", function(data, prevChildKey) {
-    var Marker = data.val();
-    console.log(Marker);
-    ctl.mapSrv.showMarker(Marker.dateStart,Marker.dateFinish,Marker.icon,Marker.lat,Marker.lng);
-    console.log("Ho aggiunto marker "+Marker.icon);
-    console.log(ctl.mapSrv.markers[ctl.mapSrv.markers.length-1]);
-    NgMap.getMap({id: 'map'}).then(function(map){
-    google.maps.event.trigger(map,'resize');
-    })
-  });
-  }
-
   
   function buildToggler(componentId) {
     return function() {
--- app-base/app-base/www/app/controller_app_map.js
+++ app-base/app-base/www/app/controller_app_map.js
@@ -1,49 +1,110 @@
-app.mapSrv = function(ctl,NgMap, NavigatorGeolocation, fine,tipo){
-	ctl.mbu = function(userId,marker) {
-  firebase.database().ref('Venezia/' + userId).set({
-     lat:marker.lat,
-     lng:marker.lng,
-     icon: marker.icona,
-     dateStart:marker.inizio,
-     //dateFinish:marker.fine
+app.mapSrv = function(ctl,NgMap, NavigatorGeolocation, fine,tipo,$scope){
+  
+  ctl.mbu = function(userId,marker) {
+    firebase.database().ref('Venezia/' + userId.uid).set({
+          lat: marker.lat, 
+          lng: marker.lng,
+          position : [marker.lat,marker.lng],
+          icona: marker.icona,
+          inizio : marker.inizio,
+          user: marker.user,
+          id: _ctl.mapSrv.markers.length
+       //dateFinish:marker.fine
+    });
+  }
+
+
+  var _ctl = ctl;
+
+  NgMap.getMap().then(function(map){
+
+    _ctl.loadMap = function(){
+      var markers = firebase.database().ref("Venezia/");
+      markers.on("child_added", function(data, prevChildKey) {
+        var Marker = data.val();
+        console.log(Marker);
+        _ctl.mapSrv.showMarker(Marker.dateStart,Marker.dateFinish,Marker.icon,Marker.lat,Marker.lng,firebase.auth().currentUser);
+        console.log("Ho aggiunto marker "+Marker.icon);
+        console.log(_ctl.mapSrv.markers[ctl.mapSrv.markers.length-1]);
+        google.maps.event.trigger(map,'resize');
+        
+      });
+    }
+
+
+    console.log("prima volta mappa",map);
+    _ctl.mapSrv = map;
+
+    console.log("ho recuperato la mappa -> ", _ctl.mapSrv==map);
+    _ctl.mapSrv.markers = [];
+    //MOSTRA I MARKER CREATI
+
+    _ctl.mapSrv.showMarker = function(dateInizio,dataFine,icona,Lat,Lng,user){
+    var x = 
+      {
+        lat: Lat, 
+        lng: Lng,
+        position : [Lat,Lng],
+        icona: icona,
+        inizio : dateInizio,
+        user: user.email,
+        id: ctl.mapSrv.markers.length
+       // fine: dataFine
+      };
+      console.log("Marker");
+      //dialogo
+      _ctl.mapSrv.markers.push(x);
+      $scope.$apply();
+    };
+
+    _ctl.mapSrv.addMarker = function(fine,tipo,iconsSet,user){
+      console.log("Dentro Marker");
+      var latlng = null;
+      var posOptions = {timeout: 10000, enableHighAccuracy: false};
+      NavigatorGeolocation.getCurrentPosition().then(function(position) {
+        latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+        var x = 
+        {
+          lat: latlng.lat, 
+          lng: latlng.lng,
+          position : [latlng.lat,latlng.lng],
+          icona: iconsSet[tipo],
+          inizio : Date.now(),
+          user: user.email,
+          id: _ctl.mapSrv.markers.length
+        };
+        _ctl.mapSrv.markers.push(x);
+        //_ctl.mbu(firebase.auth().currentUser,x);
+        console.log("Ho aggiunto marker "+tipo);
+        console.log(ctl.mapSrv.markers[ctl.mapSrv.markers.length-1])
+      });
+    }
+
+
+    _ctl.showDetail = function(event ,marker) {
+      console.log("mi hanno dato questo evento",event);
+      console.log("mi hanno dato questo marker",marker);
+      console.log(marker.id);
+      var messaggio;
+      for(var i=0; i < ctl.msg.length; i++) {
+        if(ctl.msg[i].icona== marker.icon){          
+          messaggio=ctl.msg[i].msg;
+          break;
+        }
+      }
+      _ctl.selectedMarker = marker;
+      _ctl.selectedMarker.messaggio = messaggio;
+
+      _ctl.mapSrv.showInfoWindow('foo-iw', this);
+    }
+
+    _ctl.hideDetail = function() {
+      _ctl.mapSrv.hideInfoWindow('foo-iw');
+    }
+
+    _ctl.loadMap();
+
   });
 }
 
-  ctl.mapSrv = {};
-  ctl.mapSrv = NgMap.getMap();
-  console.log("ho recuperato la mappa -> ", ctl.mapSrv);
-  ctl.mapSrv.markers = [];
-  //MOSTRA I MARKER CREATI
-  ctl.mapSrv.showMarker = function(dateInizio,dataFine,icona,Lat,Lng){
-  var x = 
-  	{
-      lat: Lat, 
-      lng: Lng,
-      icona: icona,
-      inizio : dateInizio,
-     // fine: dataFine
-  	};
-  	console.log("Marker");
-  	ctl.mapSrv.markers.push(x);
-  }
 
-  ctl.mapSrv.addMarker = function(fine,tipo,iconsSet){
-  	var latlng = null;
-  	NavigatorGeolocation.getCurrentPosition().then(function(position) {
-    	latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
- 
-  	var x = 
-  	{
-      lat: latlng.lat, 
-      lng: latlng.lng,
-      icona: iconsSet[tipo],
-      inizio : Date.now(),
-      fine: new Date(fine)
-  	};
-  	ctl.mapSrv.markers.push(x);
-  	ctl.mbu(firebase.auth().currentUser.uid,x);
-    console.log("Ho aggiunto marker "+tipo);
-    console.log(ctl.mapSrv.markers[ctl.mapSrv.markers.length-1]);
-  });
-  }
-}
